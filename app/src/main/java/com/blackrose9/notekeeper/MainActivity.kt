b0a345de0e220bad05b221c7d29933c8550d704d
blackrose9 <sarah.mwendwa@outlook.com>
2020-06-25 00:31:52 +0300

    add custom toolbar and call it in all the fragments... not sure if this is the way to do this but hey...

--- app/src/main/java/com/blackrose9/notekeeper/MainActivity.kt
+++ app/src/main/java/com/blackrose9/notekeeper/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.toolbar.*
 
 class MainActivity : AppCompatActivity() {
 
